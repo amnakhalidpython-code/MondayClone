4fd3e1a3e9e950f3e7c126d13aa64277031ee974
Hassaan9744 <128718198+Hassaan9744@users.noreply.github.com>
2026-01-04 15:12:19 +0500

    cors fixes

--- index.js
+++ index.js
@@ -19,23 +19,24 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",  
-      "http://localhost:3000",
-      "https://monday-clone-frontend.vercel.app",
-      "https://monday-frontend-one.vercel.app",
-      "https://monday-clone-nextjs-nu.vercel.app/"
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://monday-clone-frontend.vercel.app",
+    "https://monday-frontend-one.vercel.app",
+    "https://monday-clone-nextjs-nu.vercel.app",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));  
 app.use(express.json());
 
+ 
 
 
 // Routes
